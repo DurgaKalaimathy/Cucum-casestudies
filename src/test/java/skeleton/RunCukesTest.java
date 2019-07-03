891a0bfb742da64b1b24a258d6aa2dae99b8a057
DurgaKalaimathy <40620030+DurgaKalaimathy@users.noreply.github.com>
2019-07-03 15:20:34 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features= {"src/test/resources/skeleton/typematch.feature"},
+		features= {"src/test/resources/skeleton/Login.feature"},
 		glue="skeleton",
 		plugin="html:target/html-report"
 )
